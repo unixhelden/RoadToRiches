edb3e13643b9e3ffe4f9f889ce8f44243fdaf134
Stefan Held <stefan.held@unixhelden.de>
2025-12-28 18:16:19 +0100

    Fix error in Build Process

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
-// build.rs
 fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("icon.ico"); // Hier der Name deiner Icon-Datei
+        // Hier sicherstellen, dass der Pfad zu deinem Assets-Ordner passt
+        res.set_icon("assets/icon.ico"); 
         res.compile().unwrap();
     }
 }
